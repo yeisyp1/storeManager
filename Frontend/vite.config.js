2d12e9d3624cc51bd6307e7eb76ab0337fe0141b
yeisyp1 <jeipanameno@gmail.com>
2025-10-09 13:07:06 -0500

    Agregar configuración del servidor en vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -10,4 +10,8 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  server: {
+    port: 5174, 
+    cors: true   
+  }
 });
